703fb2da986eac3541c5a65a70657eab28f18132
Amanda Leaders <alsleaders@gmail.com>
2019-06-20 23:36:28 -0400

    viewport change works, display next

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import React, { useState } from 'react'
+import axios from 'axios'
+import ReactMapGL from 'react-map-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
-class App extends Component {
-  render() {
-    return <HelloWorld />
-  }
+const TOKEN =
+  'pk.eyJ1IjoiYWxzbGVhZGVycyIsImEiOiJjang1aXNrcGkwMmR5M3lsZzg4OXFyNWRqIn0.qQib-cz84tOegHyTyc0U9g'
+
+export default function HelloWorld() {
+  const [view, setView] = useState({
+    width: '100vw',
+    height: '100vh',
+    latitude: 27.7577,
+    longitude: -88.4376,
+    zoom: 12
+  })
+
+  return (
+    <>
+      <ReactMapGL
+        {...view}
+        mapboxApiAccessToken={TOKEN}
+        onViewportChange={view => {
+          setView(view)
+        }}
+      >
+        <h1>Map that will take input</h1>
+      </ReactMapGL>
+    </>
+  )
 }
-
-export default App
--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,51 +1,45 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
+import ReactMapGL from 'react-map-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
-const MY_KEY = '6w6euCCRI88Lz6cJIMDx9UCoLYyABMwo'
+// const MY_KEY = '6w6euCCRI88Lz6cJIMDx9UCoLYyABMwo'
+
+const TOKEN =
+  'pk.eyJ1IjoiYWxzbGVhZGVycyIsImEiOiJjang1aXNrcGkwMmR5M3lsZzg4OXFyNWRqIn0.qQib-cz84tOegHyTyc0U9g'
 
 //   /* <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=${MY_KEY}&libraries=places"></script>
 // const API_URL =
 //   'https://maps.googleapis.com/maps/api/place/findplacefromtext/json' */
 
-class HelloWorld extends Component {
-  state = {
-    array: []
-  }
+export default function HelloWorld() {
+  const [view, setView] = useState({
+    width: '100vw',
+    height: '100vh',
+    latitude: 27.7577,
+    longitude: -88.4376,
+    zoom: 8
+  })
 
-  componentDidMount() {
-    axios
-      .get(
-        `https://www.mapquestapi.com/search/v4/place?sort=relevance&feedback=false&key=${MY_KEY}`
-      )
-      .then(resp => resp.json())
-      .then(data => {
-        this.setState({
-          array: data.searchResults
-        })
-      })
-  }
+  // componentDidMount() {
+  //   axios
+  //     .get()
+  //     .then(resp => resp.json())
+  //     .then(data => {
+  //       this.setState({
+  //         array: data.searchResults
+  //       })
+  //     })
+  // }
 
-  render() {
-    return (
-      <>
-        <h1>Hello, World!</h1>
-
-        <ul>
-          {this.state.array.map(place => {
-            return (
-              <li>
-                {place.name}
-                <p>did this work?</p>
-              </li>
-            )
-          })}
-        </ul>
-      </>
-    )
-  }
+  return (
+    <>
+      <ReactMapGL {...view} mapboxApiAccessToken={TOKEN}>
+        <h1>Map that will take input</h1>
+      </ReactMapGL>
+    </>
+  )
 }
-
-export default HelloWorld
 
 // AIzaSyBb9ARD8xZdXaSNZXxq-aaVem1gjK5vgQ4
 // AIzaSyCrA3wbLTGgRfbH7QfumI7t9VDer3Q9QBw
