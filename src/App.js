ff048253ae9de68e9feaeb7300eab6bd31962e01
Amanda Leaders <alsleaders@gmail.com>
2019-06-25 16:26:05 -0400

    nav and header added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import axios from 'axios'
-import ReactMapGL, { Marker, Popup } from 'react-map-gl'
+import ReactMapGL, { Marker, Popup, NavigationControl } from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import cityData from './data/cities.json'
+import Headers from './components/Headers'
 
 const TOKEN =
   'pk.eyJ1IjoiYWxzbGVhZGVycyIsImEiOiJjang1aXNrcGkwMmR5M3lsZzg4OXFyNWRqIn0.qQib-cz84tOegHyTyc0U9g'
@@ -11,12 +12,13 @@ export default function HelloWorld() {
   const [view, setView] = useState({
     latitude: 27.9506,
     longitude: -82.4572,
-    zoom: 8
+    zoom: 4
   })
   const [selectedInfo, setSelectedInfo] = useState(null)
 
   return (
     <>
+      <Headers />
       <ReactMapGL
         {...view}
         width="100vw"
@@ -27,7 +29,9 @@ export default function HelloWorld() {
           setView(view)
         }}
       >
-        <h1>Map that will take input</h1>
+        <div style={{ position: 'absolute', left: 0 }}>
+          <NavigationControl />
+        </div>
         {cityData.map(city => {
           return (
             <Marker
